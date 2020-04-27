0d77741eb2034ea16b254f9db0d872877da4c12f
0deepsleep <0deepsleep@gmail.com>
2020-04-27 20:16:39 +0900

    로그인 버튼 -> ID/PW 가지고 서버에 로그인 요청 ->응답 로그로 확인

--- app/src/main/java/kun/hee/api_practice_login/MainActivity.kt
+++ app/src/main/java/kun/hee/api_practice_login/MainActivity.kt
@@ -2,7 +2,10 @@ package kun.hee.api_practice_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kun.hee.api_practice_login.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -16,6 +19,17 @@ class MainActivity : BaseActivity() {
         loginBtn.setOnClickListener {
             val inputId = idEdt.text.toString() //아이디가 존재하지 않는다면 ?
             val inputPw = pwEdt.text.toString() //비밀번호가 틀리다면 ?
+
+//          서버로 로그인 요청 => ServerUtil클래스 기능 활용
+             ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler{
+                 override fun onResponse(json: JSONObject) {
+//                   실제로 응답을 받은 걸 분석해서 => 대응 하는 코드
+
+//                  임시로 서버 응답 확인하기 위한 코드
+                     Log.d("서버응답JSON", json.toString())
+                 }
+
+             })
         }
 
     }
